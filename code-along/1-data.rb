99469a05593853f4c8088fcfddcbbf120796d219
Dhruvk-labs <dhruv.kochhar@kellogg.northwestern.edu>
2026-01-29 01:08:07 +0000

    1-data.rb updates

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,35 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
-
+puts 4
+puts 2
+puts 1 + 3
+puts 14/2
+puts 5.0/3
+puts 5 % 3
 # Non-numbers
 
+puts 'Hello,world'
+
 # True, False
+
+puts true
+puts false
 
 # Nothing
 
 # Variables
-
+x =4
+y =2 
+puts x * y
+food = 'tacos'
+puts food
 # Combine strings and variables
+puts food + ' I want to eat'
+
+puts "#{food} are delish"
+
+puts food * 3
 
 # String manipulation
+puts food.upcase
